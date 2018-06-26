e629367594191e33570d8d2e3426393a16cc7ab4
Vicki Hamm <vicki.cello@gmail.com>
2018-06-26 19:06:35 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,20 +14,20 @@ class Artist
     song.artist = self
     @@song_count += 1
   end
-  
+
   def add_song_by_name(name)
     song = Song.new(name)
     self.songs << song
     song.artist = self
     @@song_count += 1
   end
-  
+
     def songs
       @songs
     end
 
   def self.song_count
-    @songs.count
+    @@songs_count
   end
 
 
