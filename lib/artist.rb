fccf67b3514cfdde878516fd81b4ebb235ee10f5
Vicki Hamm <vicki.cello@gmail.com>
2018-06-26 18:40:07 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,21 @@
 class Artist
 
-  attr_accessor 
+  attr_accessor
 
-  def initialize
-
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
 
   end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+    song
+  end
+
 
 
 
