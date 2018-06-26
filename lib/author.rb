33dce2806860cbc4f11dbdd0109c889c2a0b2763
Vicki Hamm <vicki.cello@gmail.com>
2018-06-26 19:10:33 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,15 +15,18 @@ class Author
     @@post_count += 1
   end
 
-  def posts
-    @posts
-  end
-
   def add_post_by_title(title)
     post = Post.new(title)
     self.posts << post
     post.author = self
     @@post_count += 1
   end
+  
+    def posts
+      @posts
+    end
 
+    def self.post_count
+      @@post_count
+    end
 end
